3560afed2f481a099205c96b55ef15567c206162
vhs_s <vhs_ssr@outlook.com>
2023-06-12 19:27:29 +0200

    added gradle build

---
+++ java/src/main/java/vhsssr/leetcode/TwoSum.java
@@ -0,0 +1,13 @@
+package vhsssr.leetcode;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int tempSum;
+
+        for (int i=0; i < nums.length -1; i++){
+            tempSum=nums[i]+nums[i+1];
+            System.out.println(tempSum);
+        }
+        return null;
+    }
+}
---
+++ java/src/test/java/TwoSumTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import vhsssr.leetcode.TwoSum;
+
+public class TwoSumTest {
+
+    @Test
+    public void twoSumTest1(){
+        int[] abc = {2,7,11,15};
+        int[] expected = {0,1};
+        Assertions.assertArrayEquals(TwoSum.twoSum(abc,9),expected);
+    }
+    @Test
+    public void twoSumTest2(){
+        int[] abc = {3,2,4};
+        int[] expected = {1,2};
+        Assertions.assertArrayEquals(TwoSum.twoSum(abc,6),expected);
+    }
+    @Test
+    public void twoSumTest3(){
+        int[] abc = {3,3};
+        int[] expected = {0,1};
+        Assertions.assertArrayEquals(TwoSum.twoSum(abc,6),expected);
+    }
+}
